ec0964461861f4fcac50f51eba34edb0ff2fa609
Connor Meredith <connor.meredith0913@gmail.com>
2021-11-13 14:26:50 -0500

    Add docstrings to the Point class.

--- src/DimensionReduction/Point.scala
+++ src/DimensionReduction/Point.scala
@@ -2,7 +2,16 @@ package DimensionReduction
 
 import scala.math.sqrt
 
+/** Represents a point in space of arbitrary dimension.
+ *
+ *  The primary purpose of this class is to augment Vector[Double] with helper functions and attributes.
+ *  Operations are generally only well defined for points of the same dimension. Most functions in this
+ *  class therefore return Option[Point]
+ *
+ *  @param coordinates The coordinates of the point.
+ */
 class Point(val coordinates: Vector[Double]) {
+  /** The dimension of the point. Equal to the length of the coordinates vector. */
   val dimension: Int = coordinates.length
 
   private def applyCoordinatewise(other: Point)(op: (Double, Double) => Double): Option[Point] =
@@ -15,19 +24,25 @@ class Point(val coordinates: Vector[Double]) {
     }
   private def applyCoordinatewise(op: Double => Double): Point = new Point(coordinates.map(op))
 
+  /** (Optionally) Returns the vector sum of this with other */
   def +(other: Point): Option[Point] = applyCoordinatewise(other)(_ + _)
 
+  /** Returns the scalar product of s with this */
   def *(s: Double): Point = applyCoordinatewise(_ * s)
+  /** (Optionally) Returns the coordinatewise product of this with other */
   def *(other: Point): Option[Point] = applyCoordinatewise(other)(_ * _)
 
+  /** (Optionally) Returns the vector difference of this with other */
   def -(other: Point): Option[Point] = this + (other * -1.0)
 
+  /** (Optionally) Returns the square of the distance between this and other */
   def distSquared(other: Point): Option[Double] = {
     val difference: Option[Point] = this - other
     val differenceSquared: Option[Point] = difference.flatMap(p => p * p)
     differenceSquared.map(_.coordinates.sum)
   }
 
+  /** (Optionally) Returns the distance between this and other */
   def dist(other: Point): Option[Double] = distSquared(other).map(sqrt)
 
   override def toString: String =
