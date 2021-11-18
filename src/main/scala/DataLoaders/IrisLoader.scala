47f37f3e61fb62d4d3627502edb664c22d887469
Connor Meredith <connor.meredith0913@gmail.com>
2021-11-18 14:28:08 -0500

    Add normalization and Double type options.

--- src/main/scala/DataLoaders/IrisLoader.scala
+++ src/main/scala/DataLoaders/IrisLoader.scala
@@ -1,6 +1,7 @@
 package DataLoaders
 
-import DimensionReduction.Point
+import DimensionReduction.Delaunay.{DelaunayUtil, PointedAffineSpace}
+import DimensionReduction.{GeometricSimplicialComplex, Point, RandomDistStats, Simplex}
 
 import scala.io.{BufferedSource, Source}
 
@@ -13,7 +14,7 @@ object IrisLoader {
    *
    *  @return The Iris dataset
    */
-  def getIrisData: (Vector[Vector[Float]], Vector[Vector[Float]]) = {
+  def getIrisData(normalizeToHypercube: Boolean = false): (Vector[Vector[Float]], Vector[Vector[Float]]) = {
     val bufferedIrisSource: BufferedSource = Source.fromFile("Datasets/iris.data")
     val irisData: Vector[IrisDatum] = bufferedIrisSource.getLines().map(line => {
       val entries: Array[String] = line.split(",")
@@ -30,7 +31,43 @@ object IrisLoader {
       case _ => Vector(0.0f, 0.0f, 0.0f)
     }
 
-    irisData.map(p => (p.coordinates.map(_.toFloat), nameToColor(p.name))).unzip
+    val minCorner: Point = Point(irisData.map(p => p.coordinates).transpose.map(_.min))
+    val maxCorner: Point = Point(irisData.map(p => p.coordinates).transpose.map(_.max))
+
+    def mapToHypercube(v: Vector[Double]): Vector[Double] = {
+      ((Point(v) - minCorner).get / (maxCorner - minCorner).get).get.coordinates
+    }
+
+
+
+    irisData.map(p => (if (normalizeToHypercube) mapToHypercube(p.coordinates).map(_.toFloat) else p.coordinates.map(_.toFloat), nameToColor(p.name))).unzip
+  }
+
+  def getIrisCoords(normalizeToHypercube: Boolean = false): (Vector[Vector[Double]], Vector[Vector[Float]]) = {
+    val bufferedIrisSource: BufferedSource = Source.fromFile("Datasets/iris.data")
+    val irisData: Vector[IrisDatum] = bufferedIrisSource.getLines().map(line => {
+      val entries: Array[String] = line.split(",")
+      val coordinates: Vector[Double] = entries.reverse.tail.reverse.map(_.toDouble).toVector
+      val name: String = entries.reverse.head
+      new IrisDatum(coordinates, name)
+    }).toVector.distinctBy(_.coordinates)
+    bufferedIrisSource.close()
+
+    val nameToColor: String => Vector[Float] = {
+      case "Iris-setosa" => Vector(1.0f, 0.0f, 0.0f)
+      case "Iris-versicolor" => Vector(0.0f, 1.0f, 0.0f)
+      case "Iris-virginica" => Vector(0.0f, 0.0f, 1.0f)
+      case _ => Vector(0.0f, 0.0f, 0.0f)
+    }
+
+    val minCorner: Point = Point(irisData.map(p => p.coordinates).transpose.map(_.min))
+    val maxCorner: Point = Point(irisData.map(p => p.coordinates).transpose.map(_.max))
+
+    def mapToHypercube(v: Vector[Double]): Vector[Double] = {
+      ((Point(v) - minCorner).get / (maxCorner - minCorner).get).get.coordinates
+    }
+
+    irisData.map(p => (if (normalizeToHypercube) mapToHypercube(p.coordinates) else p.coordinates, nameToColor(p.name))).unzip
   }
 
 }
