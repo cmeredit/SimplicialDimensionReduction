616ce09334abe0386315256585bfcd6fef60d16c
Connor Meredith <connor.meredith0913@gmail.com>
2021-11-19 09:57:05 -0500

    Extend global basis computation away from the codimension 1 case. Deal with dim 0 case.

--- src/main/scala/DimensionReduction/Simplex.scala
+++ src/main/scala/DimensionReduction/Simplex.scala
@@ -9,11 +9,13 @@ case class Simplex(points: Set[Point]) {
 
   private lazy val ptsVec: Vector[Point] = points.toVector
 
-  private lazy val normalVec: Vector[Rational] = LinearUtil.getUnitNormalVector({
-    val basePoint: Point = ptsVec.head
-    val tailPoints: Vector[Point] = ptsVec.tail
-    tailPoints.flatMap(_ - basePoint)
-  }).coordinates.map(Rational(_))
+  val dimension: Int = ptsVec.length - 1
+
+//  private lazy val normalVec: Vector[Rational] = LinearUtil.getUnitNormalVector({
+//    val basePoint: Point = ptsVec.head
+//    val tailPoints: Vector[Point] = ptsVec.tail
+//    tailPoints.flatMap(_ - basePoint)
+//  }).coordinates.map(Rational(_))
 
   private lazy val localBasis: Vector[Vector[Rational]] = {
     val basePoint: Point = ptsVec.head
@@ -22,35 +24,57 @@ case class Simplex(points: Set[Point]) {
     differences.map(pt => pt.coordinates.map(Rational(_)))
   }
 
-  private lazy val changeOfBasisMatrix: Vector[Vector[Rational]] = LinearUtil.getInverse((LinearUtil.extendToBasis(localBasis)).transpose).get
+  private lazy val localDimension: Int = localBasis.length
+  private lazy val globalBasis: Vector[Vector[Rational]] = LinearUtil.extendToBasis(localBasis)
+
+//  println("Points:")
+//  ptsVec foreach println
+//  println("Dimension: " + dimension)
+//  println("Local basis:")
+//  localBasis foreach println
+//  println("Global basis:")
+//  globalBasis foreach println
+
+  private lazy val changeOfBasisMatrix: Vector[Vector[Rational]] = LinearUtil.getInverse(globalBasis.transpose).get
 
   def containsPoint(p: Point, tolerance: Rational = Rational(0.0001)): Boolean = {
 
-    val pColumnVec: Vector[Vector[Rational]] = Vector((p - basePoint).get.map(Rational(_))).transpose
+    if (localDimension > 0 ) {
+      val pColumnVec: Vector[Vector[Rational]] = Vector((p - basePoint).get.map(Rational(_))).transpose
 
-    val pWRTThisBasisCol: Vector[Vector[Rational]] = LinearUtil.matrixMult(changeOfBasisMatrix, pColumnVec)
+      val pWRTThisBasisCol: Vector[Vector[Rational]] = LinearUtil.matrixMult(changeOfBasisMatrix, pColumnVec)
 
-//    pWRTThisBasisCol foreach println
+      //    pWRTThisBasisCol foreach println
 
-    val pWRTThisBasis: Vector[Rational] = pWRTThisBasisCol.transpose.head
+      val pWRTThisBasis: Vector[Rational] = pWRTThisBasisCol.transpose.head
 
-//    println(pWRTThisBasis)
+      //    println(pWRTThisBasis)
 
-//    println("")
-//    println("Conditions:")
-//    println("Last entry:")
-//    println(pWRTThisBasis.last.abs.toDouble)
-//    println(pWRTThisBasis.last.abs < tolerance)
-//    println("Point with respect to simplex localBasis:")
-//    println(pWRTThisBasis.map(_.toDouble))
-//    println(pWRTThisBasis.forall(_ >= Rational(0.0)))
-//    println("Sum of coordinates:")
-//    println(pWRTThisBasis.reduce(_ + _).abs.toDouble)
-//    println(pWRTThisBasis.reduce(_ + _) < Rational(1.0) + tolerance)
-//    println("")
+      //    println("")
+      //    println("Conditions:")
+      //    println("Last entry:")
+      //    println(pWRTThisBasis.last.abs.toDouble)
+      //    println(pWRTThisBasis.last.abs < tolerance)
+      //    println("Point with respect to simplex localBasis:")
+      //    println(pWRTThisBasis.map(_.toDouble))
+      //    println(pWRTThisBasis.forall(_ >= Rational(0.0)))
+      //    println("Sum of coordinates:")
+      //    println(pWRTThisBasis.reduce(_ + _).abs.toDouble)
+      //    println(pWRTThisBasis.reduce(_ + _) < Rational(1.0) + tolerance)
+      //    println("")
 
+      val localCoordinatesOfP: Vector[Rational] = pWRTThisBasis.take(localDimension)
+      val extraCoordinatesOfP: Vector[Rational] = pWRTThisBasis.drop(localDimension)
 
-    (pWRTThisBasis.last.abs < tolerance) && pWRTThisBasis.forall(_ >= Rational(0.0)) && (pWRTThisBasis.reduce(_ + _) < Rational(1.0) + tolerance)
+      extraCoordinatesOfP.forall(_.abs < tolerance) && localCoordinatesOfP.forall(_ >= Rational(0.0)) && (localCoordinatesOfP.reduce(_ + _) < Rational(1.0) + tolerance)
+    } else {
+      basePoint.dist(p) match {
+        case None => false
+        case Some(dist) =>
+//          println("Distance: " + dist)
+          dist < tolerance
+      }
+    }
   }
 
   // Projection matrix
