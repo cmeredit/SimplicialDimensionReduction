d392e762463e57bcdeac18921a1c95430f39be8d
Connor Meredith <connor.meredith0913@gmail.com>
2021-11-19 10:02:09 -0500

    Write a test for the "intelligent" projection algorithm (currently passing).

--- src/main/scala/DimensionReduction/GeometricSimplicialComplex.scala
+++ src/main/scala/DimensionReduction/GeometricSimplicialComplex.scala
@@ -398,7 +398,37 @@ object GeometricSimplicialComplex {
 
 }
 
+// This set of tests currently passes. (Be warned - it takes a long time to run...)
+object ProjectionTest extends App {
+  val maybeCulledGeomComp: Option[GeometricSimplicialComplex] = GeometricSimplicialComplex.loadFromFile("Datasets/CulledNormalizedIrisComplex.txt")
 
+  assert(maybeCulledGeomComp.nonEmpty)
+
+  val culledGeomComp: GeometricSimplicialComplex = maybeCulledGeomComp.get
+
+  val numPointsToTest: Int = 100
+  val uniformHypercubePoints: Vector[Point] = (0 until numPointsToTest).map(_ => Point((0 until 4).map(_ => scala.util.Random.nextDouble()).toVector)).toVector
+
+  uniformHypercubePoints.zipWithIndex.foreach({case (point, idx) =>
+
+    println("Testing point " + (idx+1) + " of " + numPointsToTest)
+    println(point)
+
+    val projection: Point = culledGeomComp.project(point)
+    val bruteForceProjection: Point = culledGeomComp.bruteForceProject(point)
+    val projectionOfProjection: Point = culledGeomComp.project(projection)
+
+    // A projection operator should be idempotent.
+    assert(projection == projectionOfProjection)
+
+    // Our intelligent operator should produce correct results
+    assert(projection == bruteForceProjection)
+
+    // Our complex should actually contain the projections of points onto the complex.
+    assert(culledGeomComp.simplices.exists(_.containsPoint(projection)))
+  })
+
+}
 
 object LoadTest extends App {
 
